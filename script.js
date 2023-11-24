9fa04f3aeab5ff4831feeaf3b14d231d0a61f9b5
Pirathib Mahalingam <pirathib.m@gmail.com>
2023-11-24 22:39:04 +0100

    added async function

---
+++ script.js
@@ -0,0 +1,9 @@
+let currentPokemon;
+
+async function loadPokemon() {
+    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let response = await fetch(url);
+    currentPokemon = await response.json();
+    
+    
+}
