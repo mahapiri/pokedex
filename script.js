e061b70c3baa92915e2899f1f7e281e1f40a1ff9
Pirathib Mahalingam <pirathib.m@gmail.com>
2023-11-25 23:11:13 +0100

    added fetch

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 let pokemonArray = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran♀","Nidorina","Nidoqueen","Nidoran♂","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Ninetales","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra","Alakazam","Machop","Machoke","Machamp","Bellsprout","Weepinbell","Victreebel","Tentacool","Tentacruel","Geodude","Graveler","Golem","Ponyta","Rapidash","Slowpoke","Slowbro","Magnemite","Magneton","Farfetch'd","Doduo","Dodrio","Seel","Dewgong","Grimer","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Onix","Drowzee","Hypno","Krabby","Kingler","Voltorb","Electrode","Exeggcute","Exeggutor","Cubone","Marowak","Hitmonlee","Hitmonchan","Lickitung","Koffing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra","Goldeen","Seaking","Staryu","Starmie","Mr. Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp","Gyarados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops","Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo","Mew"];
 
+
+
 async function init() {
+    await loadPokemon();
     loadCard();
 }
 
 
-let currentPokemon;
-
 async function loadPokemon() {
     let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
     let response = await fetch(url);
-    currentPokemon = await response.json();
+    let currentPokemon = await response.json();
+    pokemonName = currentPokemon['name'];
+    pokemonID = currentPokemon['id'];
+    pokemonHeight = currentPokemon['height'];
+    pokemonImg = currentPokemon['sprites']['other']['dream_world']['front_default'];
+    pokemonBigImg = currentPokemon['sprites']['other']['official-artwork']['front_default'];
+    typeOfElement = currentPokemon['types']['0']['type']['name'];
+    console.log(currentPokemon);
+    console.log(typeOfElement);
 }
 
 
@@ -19,13 +28,13 @@ function loadCard() {
     content.innerHTML += /*html*/`
         <div class="card">
             <div class="card-bg"></div>
-                <h3>Hallo</h3>
-                <div>
-                    <div>
-                        <p>dsgg</p>
-                        <p>sagdasdg</p>
+                <h3 class="card-headline">${pokemonName.toUpperCase()}</h3>
+                <div class="card-undersection">
+                    <div class="info-section">
+                        <p class="info-bar">${typeOfElement}</p>
+                        <p class="info-bar">sagdasdg</p>
                     </div>
-                    <img src="" alt="">
+                    <div class="card-undersection-img"><img src="${pokemonImg}" alt=""></div>
                 </div>
         </div>  
     `;
